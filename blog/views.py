5e8d32c3f731415019dfcc4be83ecf64dc455552
AndresQuiVal <quirozval.andres@hotmail.com>
2021-07-22 13:58:45 -0500

    Remove index_view innecesary view

--- blog/views.py
+++ blog/views.py
@@ -2,18 +2,6 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post
 
-def index_view(request): # TODO: transform to generic ListView class
-    """
-    Returns the main template in the app
-    """
-    posts = Post.objects.all()
-    context = {
-        'post_list' : posts
-    }
-
-    return render(request, 'blog/index.html', context)
-
-
 class PostList(generic.ListView):
     template_name = 'blog/index.html'
     queryset = Post.objects.all()
