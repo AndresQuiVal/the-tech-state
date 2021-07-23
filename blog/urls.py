57866e4d1271d04069b9539ef73790962e81e679
AndresQuiVal <quirozval.andres@hotmail.com>
2021-07-23 16:36:37 -0500

    Implemented Posts deletion (just in User page) and removed posts.html and posts.js

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ main_path = path('', views.PostList.as_view(), name = 'index-blog-view')
 app_name = 'blog'
 urlpatterns = [
     path('dashboard/', views.PostList.as_view(), name = 'index-blog-view'),
-    path('posts/<int:pk>', views.PostDetail.as_view(), name = 'post-detail')
+    path('posts/<int:pk>', views.PostDetail.as_view(), name = 'post-detail'),
+    path('posts/<int:pk>/delete', views.delete_post, name = 'post-delete')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 from django.views import generic
 from .models import Post
 
@@ -6,6 +8,21 @@ class PostList(generic.ListView):
     template_name = 'blog/index.html'
     queryset = Post.objects.all()
 
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data(**kwargs)
+        # add username context variable
+        return context
+
 class PostDetail(generic.DetailView):
     model = Post
+
+# TODO: Change CRUD Post actions to Django REST
+
+def delete_post(request, pk): 
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    # post deleted
+    return redirect(reverse('blog:index-blog-view'))
+
+
 
