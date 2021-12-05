b482b76c1cc173734d59e76ba1355ea406d75e52
AndresQuiVal <quirozval.andres@hotmail.com>
2021-12-05 12:26:58 -0600

    Changed redirect URL login to production

--- users/static/users/js/main.js
+++ users/static/users/js/main.js
@@ -5,5 +5,5 @@ loginBtn.onclick = loginDiscord;
 
 function loginDiscord() {
     var client_id = '833554127652519957'; // process.env.DISCORD_APP_CLIENT_ID; // process.env -> object that has environment variables
-    window.location.href = `https://discord.com/api/oauth2/authorize?client_id=${client_id}&redirect_uri=http%3A%2F%2F127.0.0.1%3A8000%2Fusers%2Flogin&response_type=code&scope=identify%20email%20guilds&prompt=none`;
+    window.location.href = `https://discord.com/api/oauth2/authorize?client_id=${client_id}&redirect_uri=https%3A%2F%2Fthetechstate.herokuapp.com%2Fusers%2Flogin&response_type=code&scope=identify%20email%20guilds&prompt=none`;
 }
